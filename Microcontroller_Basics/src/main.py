a4ae44c1edf7672ec14b9b0cd596bc3e688c0acc
Sreeraaghav Raja <rajas@ufl.edu>
2026-02-01 19:24:04 -0500

    finished the lab

--- Microcontroller_Basics/src/main.py
+++ Microcontroller_Basics/src/main.py
@@ -1,14 +1,19 @@
 from machine import Pin, Timer
 
-led_pin = Pin(6, Pin.OUT)
+# Global Variable Initializations
+led_pin = Pin(1, Pin.OUT)
+piezo_pin = Pin(0, Pin.OUT)
+switch_pin = Pin("GP7", Pin.IN, Pin.PULL_UP)
 
-# initialize piezo pin
-piezo_pin = Pin(7, Pin.OUT)
-
-# initialize the switch pin
-
+# Timer Object Initializations
 tim = Timer()
+tim2 = Timer()
+tim3 = Timer()
 
+# initialize a global flag;
+global_flag = 0; 
+
+# function definitions
 def led(timer):
     global led_pin
     led_pin.toggle()
@@ -17,11 +22,40 @@ def piezo(timer):
     global piezo_pin
     piezo_pin.toggle()
     
-def main(timer):
+def switch_handler(Pin):
+    tim3.init(mode = Timer.PERIODIC, period=10, callback = switch_timer_handler)
+
+def switch_timer_handler(timer):
+    # check if the pin is released
+    if(switch_pin.value()):
+        # deinitialize the timer
+        tim3.deinit()
+        
+        # initialize global flag
+        global global_flag
+        # change flag value
+        global_flag = 1 if global_flag == 0 else 0
+        
+        if(global_flag == 0):
+            # if the button is pushed then do this thing
+            piezo_pin.off()
+            tim2.deinit()
+            tim.init(freq = 2, mode = Timer.PERIODIC, callback = led)
+            # print("Doll")
+        else:
+            # else do the piezo
+            led_pin.off()
+            tim.deinit()
+            tim2.init(freq = 2000, mode = Timer.PERIODIC, callback = piezo)
+            # print("Notes") 
     
+def main():
+        # 16 interrupt levels on RP2350 but automatically handled by the RP2350
+        switch_pin.irq(switch_handler, trigger=Pin.IRQ_FALLING)
+        tim.init(freq = 2, mode = Timer.PERIODIC, callback = led)
+        
+        while True:
+           pass
 
-# if the button is pushed then do this thing
-tim.init(freq = 2, mode = Timer.PERIODIC, callback = led)
-
-# else do the piezo
-tim.init(freq = 2000, mode = Timer.PERIODIC, callback = piezo)
+if __name__ == "__main__":
+    main()
