ab06a285919e31e0def23b57e82c8bfc9ca6e99b
Sreeraaghav-22 <rajas@ufl.edu>
2026-04-02 15:27:03 -0400

    set up everything and will now seriously work on the project

---
+++ Final_Project/Junior_Design_C/build/CMakeFiles/final_project.dir/home/sreer/pico/pico-sdk/lib/lwip/src/core/tcp_in.c.o.d
@@ -0,0 +1,82 @@
+CMakeFiles/final_project.dir/home/sreer/pico/pico-sdk/lib/lwip/src/core/tcp_in.c.o: \
+ /home/sreer/pico/pico-sdk/lib/lwip/src/core/tcp_in.c \
+ /home/sreer/pico/pico-sdk/lib/lwip/src/include/lwip/opt.h \
+ /home/sreer/Junior_Design/Final_Project/Junior_Design_C/src/lwipopts.h \
+ /home/sreer/pico/pico-sdk/lib/lwip/src/include/lwip/debug.h \
+ /home/sreer/pico/pico-sdk/lib/lwip/src/include/lwip/arch.h \
+ /home/sreer/pico/pico-sdk/src/rp2_common/pico_lwip/include/arch/cc.h \
+ /usr/include/newlib/sys/time.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stddef.h \
+ /usr/include/newlib/sys/_timeval.h /usr/include/newlib/sys/_types.h \
+ /usr/include/newlib/machine/_types.h /usr/include/newlib/sys/types.h \
+ /usr/include/newlib/sys/_stdint.h /usr/include/newlib/machine/endian.h \
+ /usr/include/newlib/machine/_endian.h /usr/include/newlib/sys/select.h \
+ /usr/include/newlib/sys/_sigset.h /usr/include/newlib/sys/timespec.h \
+ /usr/include/newlib/sys/_timespec.h \
+ /usr/include/newlib/sys/_pthreadtypes.h /usr/include/newlib/sys/sched.h \
+ /usr/include/newlib/machine/types.h /usr/include/newlib/time.h \
+ /usr/include/newlib/_ansi.h /usr/include/newlib/sys/reent.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/machine/time.h \
+ /usr/include/newlib/sys/_locale.h /usr/include/newlib/machine/_time.h \
+ /home/sreer/pico/pico-sdk/src/common/pico_base_headers/include/pico.h \
+ /home/sreer/pico/pico-sdk/src/common/pico_base_headers/include/pico/types.h \
+ /home/sreer/pico/pico-sdk/src/common/pico_base_headers/include/pico/assert.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdbool.h \
+ /usr/include/newlib/assert.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h \
+ /home/sreer/Junior_Design/Final_Project/Junior_Design_C/build/generated/pico_base/pico/version.h \
+ /home/sreer/pico/pico-sdk/src/common/pico_base_headers/include/pico/config.h \
+ /home/sreer/Junior_Design/Final_Project/Junior_Design_C/build/generated/pico_base/pico/config_autogen.h \
+ /home/sreer/pico/pico-sdk/src/boards/include/boards/pico2_w.h \
+ /home/sreer/pico/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h \
+ /home/sreer/pico/pico-sdk/src/rp2350/pico_platform/include/pico/platform.h \
+ /home/sreer/pico/pico-sdk/src/rp2_common/pico_platform_compiler/include/pico/platform/compiler.h \
+ /home/sreer/pico/pico-sdk/src/rp2350/hardware_regs/include/hardware/platform_defs.h \
+ /home/sreer/pico/pico-sdk/src/common/pico_base_headers/include/pico/types.h \
+ /home/sreer/pico/pico-sdk/src/rp2_common/pico_platform_sections/include/pico/platform/sections.h \
+ /home/sreer/pico/pico-sdk/src/rp2_common/pico_platform_panic/include/pico/platform/panic.h \
+ /home/sreer/pico/pico-sdk/src/rp2_common/pico_platform_common/include/pico/platform/common.h \
+ /home/sreer/pico/pico-sdk/src/rp2350/hardware_regs/include/hardware/regs/addressmap.h \
+ /home/sreer/pico/pico-sdk/src/rp2350/hardware_regs/include/hardware/regs/sio.h \
+ /home/sreer/pico/pico-sdk/src/common/pico_base_headers/include/pico/error.h \
+ /home/sreer/pico/pico-sdk/src/rp2_common/pico_rand/include/pico/rand.h \
+ /usr/include/newlib/stdio.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdarg.h \
+ /usr/include/newlib/sys/stdio.h /usr/include/newlib/stdlib.h \
+ /usr/include/newlib/machine/stdlib.h /usr/include/newlib/alloca.h \
+ /usr/include/newlib/inttypes.h /usr/include/newlib/sys/_intsup.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/limits.h \
+ /usr/include/newlib/ctype.h \
+ /home/sreer/pico/pico-sdk/lib/lwip/src/include/lwip/priv/tcp_priv.h \
+ /home/sreer/pico/pico-sdk/lib/lwip/src/include/lwip/tcp.h \
+ /home/sreer/pico/pico-sdk/lib/lwip/src/include/lwip/tcpbase.h \
+ /home/sreer/pico/pico-sdk/lib/lwip/src/include/lwip/mem.h \
+ /home/sreer/pico/pico-sdk/lib/lwip/src/include/lwip/pbuf.h \
+ /home/sreer/pico/pico-sdk/lib/lwip/src/include/lwip/err.h \
+ /home/sreer/pico/pico-sdk/lib/lwip/src/include/lwip/ip.h \
+ /home/sreer/pico/pico-sdk/lib/lwip/src/include/lwip/def.h \
+ /home/sreer/pico/pico-sdk/lib/lwip/src/include/lwip/ip_addr.h \
+ /home/sreer/pico/pico-sdk/lib/lwip/src/include/lwip/ip4_addr.h \
+ /home/sreer/pico/pico-sdk/lib/lwip/src/include/lwip/ip6_addr.h \
+ /home/sreer/pico/pico-sdk/lib/lwip/src/include/lwip/def.h \
+ /home/sreer/pico/pico-sdk/lib/lwip/src/include/lwip/netif.h \
+ /home/sreer/pico/pico-sdk/lib/lwip/src/include/lwip/stats.h \
+ /home/sreer/pico/pico-sdk/lib/lwip/src/include/lwip/memp.h \
+ /home/sreer/pico/pico-sdk/lib/lwip/src/include/lwip/priv/memp_std.h \
+ /home/sreer/pico/pico-sdk/lib/lwip/src/include/lwip/priv/memp_priv.h \
+ /home/sreer/pico/pico-sdk/lib/lwip/src/include/lwip/priv/mem_priv.h \
+ /home/sreer/pico/pico-sdk/lib/lwip/src/include/lwip/ip4.h \
+ /home/sreer/pico/pico-sdk/lib/lwip/src/include/lwip/prot/ip4.h \
+ /home/sreer/pico/pico-sdk/lib/lwip/src/include/lwip/ip6.h \
+ /home/sreer/pico/pico-sdk/lib/lwip/src/include/lwip/prot/ip.h \
+ /home/sreer/pico/pico-sdk/lib/lwip/src/include/lwip/icmp.h \
+ /home/sreer/pico/pico-sdk/lib/lwip/src/include/lwip/prot/icmp.h \
+ /home/sreer/pico/pico-sdk/lib/lwip/src/include/lwip/prot/tcp.h \
+ /home/sreer/pico/pico-sdk/lib/lwip/src/include/lwip/inet_chksum.h \
+ /home/sreer/pico/pico-sdk/lib/lwip/src/include/lwip/nd6.h \
+ /usr/include/newlib/string.h /usr/include/newlib/strings.h \
+ /usr/include/newlib/sys/string.h
---
+++ Final_Project/Junior_Design_C/build/CMakeFiles/final_project.dir/home/sreer/pico/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/final_project.dir/home/sreer/pico/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c.o: \
+ /home/sreer/pico/pico-sdk/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c
